191cbbd2919977ceb38bb9ac9568cf8e704d05b4
zahid0247 <zahid.mirza4702@gmail.com>
2025-11-24 12:25:04 +0530

    first commit

---
+++ Arethmetic_opration.py
@@ -0,0 +1,9 @@
+'''Performs the basic mathematical operations on those two numbers come from user'''
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+print("Addition of two number is:", a + b)
+print("Substraction of two number is:", a - b)
+print("Division of two number is:", a / b)
+print("Multiplication of two number is:", a * b)
---
+++ greeting.py
@@ -0,0 +1,10 @@
+'''Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+
+name = input("Eneter your first name:")
+last = input("Enter your last name: ")
+
+print(f"\nHello, {name} {last}! Welcome to Python program.")
